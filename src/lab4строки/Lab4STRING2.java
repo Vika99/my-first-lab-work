346e42fd62df56ddf835638b24aa3e7bc2db1b72
Vika <vikavoronchuk@mail.ru>
2020-03-23 14:07:49 +0300

    работа№4,номер2

---
+++ src/lab4строки/Lab4STRING2.java
@@ -0,0 +1,11 @@
+public class Lab4STRING2 {
+    public static void main(String[] args) {
+        //№ 2!!!!!!Найти в строке указанную подстроку и заменить ее на новую.
+        // Строку, ее подстроку для замены и новую подстроку вводит пользователь.
+        String s2 = " We must live the future!";
+        System.out.println("LastIndexOf :future" + s2.lastIndexOf("future"));
+        System.out.println("we must live the future".replace("future", "present"));
+    }
+
+
+}
